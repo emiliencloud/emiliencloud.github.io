45be12c5830a9fe716bba518df3804f2f177bed9
emiliencloud <78021941+emiliencloud@users.noreply.github.com>
2021-06-27 16:19:52 +0200

    Add files via upload

---
+++ script/anim.js
@@ -0,0 +1,28 @@
+const sr = ScrollReveal();
+
+sr.reveal('.anim1', {
+    delay : 100,
+    duration : 1500,
+    reset : true,
+    scale : 0.95
+});
+
+sr.reveal('.card1', {
+    duration : 1500,
+    reset : true,
+    scale : 0.95
+});
+
+sr.reveal('.card2', {
+    delay : 300,
+    duration : 1500,
+    reset : true,
+    scale : 0.95
+});
+
+sr.reveal('.card3', {
+    delay : 600,
+    duration : 1500,
+    reset : true,
+    scale : 0.95
+});
